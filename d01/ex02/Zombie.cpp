510f704d8bc53df231ae7e1566dd7f6970bc9813
Jan-hendrik HOON <janhoon@e4r6p2.jnb.42.fr>
2017-05-23 15:29:40 +0200

    fixed small leak

--- d01/ex02/Zombie.cpp
+++ d01/ex02/Zombie.cpp
@@ -4,7 +4,7 @@
 
 #include "Zombie.hpp"
 
-Zombie::Zombie(std::string name, std::string type) : _name(name), _type(type) {}
+Zombie::Zombie(std::string name, std::string type) : _type(type), _name(name) {}
 
 Zombie::~Zombie(void) {
     return;
